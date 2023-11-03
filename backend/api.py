6b1736eb7fb4dfdb95bf5c195925f5229b9b4add
MartijnVanZelst <mpmj.vanzelst@student.avans.nl>
2023-11-03 11:35:20 +0100

    [ADD] API For model

---
+++ backend/api.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+import pickle
+import shap
+import numpy as np
+
+app = Flask(__name__)
+
+# Load your pickled model and prepare a SHAP explainer during application startup
+with open('DecisionTreeGood.pickle', 'rb') as model_file:
+    model = pickle.load(model_file)
+
+# Define a SHAP explainer for your model
+explainer = shap.Explainer(model)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Get input data as JSON
+        data = request.json
+        features = data['features']
+
+        # Make predictions
+        prediction = model.predict([features])
+
+        # Explain the prediction using SHAP
+        shap_values = explainer.shap_values(np.array([features]))
+        
+        # Assuming prediction is binary (0 or 1), you can return the result as JSON
+        result = {"prediction": int(prediction[0]), 'shap_values': shap_values[0].tolist()}
+
+        return jsonify(result)
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
