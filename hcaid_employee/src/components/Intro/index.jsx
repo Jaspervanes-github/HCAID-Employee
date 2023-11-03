b08ec10e5b5368d8c4469c09381f052a88f95f83
Jasper <j.vanes1@student.avans.nl>
2023-11-03 13:39:11 +0100

    [ADD] made sure that every page is styled the same
    Changed the styling of the home page and the HowItWorks page

---
+++ hcaid_employee/src/components/Intro/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./index.css";
+
+
+function Intro(props) {
+    return (
+        <div className="intro">
+            <h1>Welcom to WorkforceWatch,</h1>
+            <p>With the use of this app you can predict if an employee of yours is going to quit or not!
+                Start predicting now!
+            </p>
+            <a href='/Predict'>
+            <button>Start</button>
+          </a>
+        </div >
+    )
+}
+
+export default Intro
